de6c85ce50f7764d18b520df3d70c5e8f0d3d105
Laurentiu Enache <enlau101@gmail.com>
2018-08-07 22:45:51 +0100

    update to folder structure

--- source php code/src/AppBundle/Services/Captcha/CaptchaValidatorInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types = 1);
-
-namespace App\AppBundle\Services\Captcha;
-
-interface CaptchaValidatorInterface
-{
-    public function validate(string $captcha, string $clientIp) : bool;
-}
